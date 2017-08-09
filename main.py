2a019a18bc8b42c85e05a3b7e3662dfaf03bb69e
Too-Naive <sometimes.naive@hotmail.com>
2017-08-10 02:54:23 +0800

    Using dict to replace list

--- main.py
+++ main.py
@@ -28,83 +28,83 @@ admin_type = ['creator','administrator']
 
 group_cache = None
 
-class group_list:
-	def __init__(self):
-		self.group_id = []
-		self.group_msg = []
-		self.is_admin = []
-	def append(self,x):
-		global bot,bot_id
-		self.group_id.append(x[0])
-		self.group_msg.append(x[1])
-		#self.is_admin.append(x[2])
-		self.is_admin.append(self.__check_admin(bot.getChatMember(x[0],bot_id)['status']))
-		return self.is_admin[:-1]
-	def delete(self,chat_id):
-		index = self.find_group_index(chat_id)
-		del self.group_id[index]
-		del self.group_msg[index]
-		del self.is_admin[index]
-	def find_group_index(self,chat_id):
-		for x in xrange(0,len(self.group_id)):
-			if self.group_id[x] == chat_id:
-				return x
-		return -1
-	def check_admin(self,chat_id):
-		index = self.find_group_index(chat_id)
-		assert(index != -1)
-		if self.is_admin[index] == 1:
-			return True
-		else:
-			return False
-
 class group_cache_class:
 	def __init__(self):
-		self.l = group_list()
+		self.g = dict()
 	def load(self):
 		self.__init__()
 		sql = mm(sqlhost,sqlport,sqluser,sqlpwd,sqlname)
 		result = sql.query("SELECT * FROM `welcomemsg`")
 		sql.close()
 		for x in result:
-			self.l.append(x)
-	def getmsg(self,chat_id):
-		pass
+			self.add(x)
+	def add(self,x):
+		global bot_id
+		try:
+			self.g[x[0]]={'msg' : x[1] , 'is_admin' : 
+				self.__check_admin(bot.getChatMember(x[0],bot_id)[status])}
+		except telepot.exception.TelegramError as e:
+			if e[0] == 'Bad Request: chat not found':
+				self.__db_del(x[0])
+				print('Delete not found chat:%d'%x[0])
+			else:
+				raise e
+	def delete(self,chat_id):
+		try:
+			del self.g[chat_id]
+			self.__db_del(chat_id)
+		except KeyError:
+			print('Can\'t find %d in delete()'%chat_id)
+	def get(self,chat_id):
+		try:
+			return self.g[chat_id]
+		except KeyError:
+			print('Can\'t find %d in get()'%chat_id)
+			return None
+	def __db_del(self,chat_id):
+		sql = mm(sqlhost,sqlport,sqluser,sqlpwd,sqlname)
+		sql.execute("DELETE FROM `welcomemsg` WHERE `group_id` = %d"%(chat_id))
+		sql.close()
 	def __check_admin(self,status):
 		if status in admin_type:
 			return 1
 		else:
 			return 0
+	def get_is_admin(self,chat_id):
+		if self.get(chat_id)['is_admin'] == 1:
+			return True
+		else:
+			return False
+	def edit(self,x):
+		sql = mm(sqlhost,sqlport,sqluser,sqlpwd,sqlname)
+		sql.execute("UPDATE `welcomemsg` SET `msg` = '%s' WHERE `group_id` = %d"%(x[1],x[0]))
+		sql.close()
+		self.g[x[0]]['msg'] = x[1]
+
+
 
 def onMessage(msg):	
 	global bot
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	print(content_type)
 	print(msg)
-	if content_type = 'new_chat_member' and msg['new_chat_members']['id'] == bot_id:
-		is_admin = group_cache.append((chat_id,None))
+	if content_type == 'new_chat_member' and msg['new_chat_members']['id'] == bot_id:
+		is_admin = group_cache.add((chat_id,None))
 		sql = mm(sqlhost,sqlport,sqluser,sqlpwd,sqlname)
 		sql.execute("INSERT INTO `welcomemsg` (`group_id`,`msg`,`is_admin`) VALUES (%d,NULL,%d)"%(chat_id,is_admin))
 		sql.close()
 		return
-	if content_type = 'left_chat_member' and msg['left_chat_member']['id'] == bot_id:
+	if content_type == 'left_chat_member' and msg['left_chat_member']['id'] == bot_id:
 		group_cache.delete(chat_id)
-		sql = mm(sqlhost,sqlport,sqluser,sqlpwd,sqlname)
-		sql.execute("DELETE FROM `welcomemsg` WHERE `group_id` = %d"%(chat_id))
-		sql.close()
 		return
 	if msg['chat']['type'] in group_type:
 		if content_type=='text' and command_match.match(msg['text']):
-			if bot.getChatMember(chat_id,bot.getMe()['id'])['status'] in admin_type:
-				if bot.getChatMember(chat_id,msg['from']['id'])['status'] not in admin_type:
-					bot.sendMessage(chat_id,'Permission Denied.\n你没有权限，快滚',
-						reply_to_message_id=msg['message_id'])
-					bot.restrictChatMember(chat_id,msg['from']['id'],until_date=msg['date']+60)
-					return
 			if bot.getChatMember(chat_id,msg['from']['id'])['status'] not in admin_type:
 				bot.sendMessage(chat_id,'Permission Denied.\n你没有权限，快滚',
 					reply_to_message_id=msg['message_id'])
-				if group_cache.find_group_index()
+				if group_cache.get_is_admin(chat_id):
+					bot.restrictChatMember(chat_id,msg['from']['id'],until_date=msg['date']+60)
+				return
 			result = setcommand_match.match(msg['text'])
 			if result:
 				welcomemsg = str(result.group(2))
@@ -113,44 +113,41 @@ def onMessage(msg):
 					r = urllib2.urlopen(welcomemsg)
 					welcomemsg = r.read()
 					r.close()
-				sql = mm(sqlhost,sqlport,sqluser,sqlpwd,sqlname)
-				result = sql.query("SELECT * FROM `welcomemsg` WHERE `group_id` = %d"%chat_id)
-				if not result:
-					sql.execute("INSERT INTO `welcomemsg` (`group_id`,`msg`) VALUES (%d,'%s')"%(chat_id,b64encode(welcomemsg)))
-				else:
-					sql.execute("UPDATE `welcomemsg` SET `msg` = '%s' WHERE `group_id` = %d"%(b64encode(welcomemsg),chat_id))
-				sql.close()
+				group_cache.edit((chat_id,b64encode(welcomemsg)))
 				bot.sendMessage(chat_id,"*Set welcome message to:*\n%s"%welcomemsg,
 					disable_web_page_preview=True,parse_mode='Markdown',reply_to_message_id=msg['message_id'])
 				return
 			result = clearcommand_match.match(msg['text'])
 			if result:
-				sql = mm(sqlhost,sqlport,sqluser,sqlpwd,sqlname)
-				result = sql.query("SELECT * FROM `welcomemsg` WHERE `group_id` = %d"%chat_id)
-				if result:
-					sql.execute("UPDATE `welcomemsg` SET `msg` = NULL WHERE `group_id` = %d"%chat_id)
-				sql.close()
-				bot.sendMessage(chat_id,"*Clear welcome message success!*",
+				group_cache.edit((chat_id,None))
+				bot.sendMessage(chat_id,"*Clear welcome message successfully!*",
 					parse_mode='Markdown',reply_to_message_id=msg['message_id'])
 				return
 			result = reloadcommand_match.match(msg['text'])
 			if result:
-
+				group_cache.load()
+				bot.sendMessage(chat_id,"*Reload configuration successfully!*",
+					parse_mode='Markdown',reply_to_message_id=msg['message_id'])
+				return
+			bot.sendMessage(chat_id,'*Current chat_id:%d*'%chat_id,
+				parse_mode='Markdown',reply_to_message_id=msg['message_id'])
 		elif content_type in content_type_concerned:
-			sql = mm(sqlhost,sqlport,sqluser,sqlpwd,sqlname)
-			result = sql.query("SELECT `msg` FROM `welcomemsg` WHERE `group_id` = %d"%chat_id)
-			sql.close()
+			result = group_cache.get(chat_id)['message']
 			if result:
 				bot.sendMessage(chat_id,b64decode(result[0][0]),parse_mode='Markdown',
 					disable_web_page_preview=True,reply_to_message_id=msg['message_id'])
 
 def main():
 	global bot,group_cache,bot_id
+	print('Start Main()')
 	bot = telepot.Bot(botToken)
+	print('Success login with token %s***********************%s'%(botToken[:4],botToken[-4:]))
 	bot.message_loop(onMessage)
+	print('message_loop() started\nGet bot id....')
 	bot_id = bot.getMe()['id']
 	group_cache = group_cache_class()
 	group_cache.load()
+	print('bot init finished')
 	while True:
 		bot.getMe()
 		time.sleep(10)
