46095ece0eeb2c3d57a01c50586851474a5eb96e
Too-Naive <sometimes.naive@hotmail.com>
2017-10-04 03:02:41 +0800

    Append Log.py features

--- botlib/Log.py
+++ botlib/Log.py
@@ -7,14 +7,23 @@ from botlib.Config import Config
 printLock = Lock()
 logFile = Config.log.logfile and open(Config.log.logfile, 'a')
 
+if Config.log.log_debug:
+	assert(Config.log.debug_lvl>=1)
+
 def info(fmt, *args, **kwargs):
 	log('INFO', Config.log.log_info, Config.log.print_info, fmt.format(*args), **kwargs)
 
 def error(fmt, *args, **kwargs):
 	log('ERROR', Config.log.log_err, Config.log.print_err, fmt.format(*args), **kwargs)
 
-def debug(fmt, *args, **kwargs):
-	log('DEBUG', Config.log.log_debug, Config.log.print_debug, fmt.format(*args), **kwargs)
+def get_debug_info():
+	if Config.log.log_debug:
+		return (True,Config.log.debug_lvl)
+
+def debug(level ,fmt, *args, **kwargs):
+	assert(type(level) is int)
+	if level <= Config.log.debug_lvl:
+		log('DEBUG', Config.log.log_debug, Config.log.print_debug, fmt.format(*args), **kwargs)
 
 def reopen(path):
 	global logFile
